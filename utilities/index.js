d8ef928b5a0b6e6c29eab93034dcf2ff1e292eb7
Ola Ajibola <kelajibola@gmail.com>
2025-03-21 17:40:38 +0100

    week 3 completed ish

--- utilities/index.js
+++ utilities/index.js
@@ -22,7 +22,7 @@ Util.getNav = async function (req, res, next) {
 * Build the classification view HTML
 * ************************************ */
 Util.buildClassificationGrid = async function(data){
-    let grid
+    // let grid
     if(data.length > 0){
       grid = '<ul id="inv-display">'
       data.forEach(vehicle => { 
@@ -50,5 +50,36 @@ Util.buildClassificationGrid = async function(data){
     }
     return grid
   }
+
+// GET Witty message for 404 Page
+Util.getWittyMessage = () =>{
+  const funny404Messages = [
+    ["Out of Gas!", "Uh-oh, this page ran out of fuel. Ready to refuel at the home page?"],
+    ["Wrong Turn!", "Looks like you took the scenic route to nowhere. Let’s head back to the dealership!"],
+    ["Engine Stalled!", "The page you’re looking for didn’t pass inspection. Let’s tow you back to safety."],
+    ["Flat Tire!", "This page hit a bump and couldn’t make it. Time to change direction!"],
+    ["Road Closed!", "Construction ahead! This page isn’t ready yet. Let’s detour to somewhere better."],
+    ["GPS Signal Lost!", "We can’t find this location. Recalculating your route back home!"],
+    ["No Vehicles Beyond This Point!", "This page is off-limits, even for off-roaders. Back it up!"],
+    ["Speed Limit Exceeded!", "You went too fast and missed the exit. Let’s slow down and head back."],
+    ["Parking Lot Full!", "There’s no space here! Let’s cruise back to the main lot."],
+    ["Out of Service!", "This page is in the shop for repairs. Come back later or return to the home page."]
+  ];
+  const funnyMessage = funny404Messages[Math.floor(Math.random() * funny404Messages.length)]
+  const funny404HTML = `<div class="error-msg">
+                          <span class="error-msg-main">${funnyMessage[0]}</span><br>
+                          <span class="error-msg-tagline">${funnyMessage[1]}</span>
+                        </div>`
+
+  return funny404HTML
+}
+
+
+/* **************************************
+* Middleware For Handling Errors
+* Wrap other function in this for
+* General Error Handling
+*************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
 module.exports = Util
